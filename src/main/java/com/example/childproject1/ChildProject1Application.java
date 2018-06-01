be5b04ed96c83278604e0cf64e5c020398fc93a0
Arun Attupurath <arun.attupurath@gmail.com>
2018-06-01 17:43:06 +0530

    first commit

---
+++ src/main/java/com/example/childproject1/ChildProject1Application.java
@@ -0,0 +1,12 @@
+package com.example.childproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildProject1Application.class, args);
+	}
+}

4601dce7fedb0482b2be57b24034305825721d0b
Arun Attupurath <arun.attupurath@gmail.com>
2018-06-02 14:45:26 +0530

    2nd commit

--- src/main/java/com/example/childproject1/ChildProject1Application.java
+++ src/main/java/com/example/childproject1/ChildProject1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChildProject1Application {
 
 	public static void main(String[] args) {
+		System.out.println("Hello1");
 		SpringApplication.run(ChildProject1Application.class, args);
 	}
 }
